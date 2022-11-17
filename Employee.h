bc6869e7118f4a73d8b59c1021cfa9cf5a1c246a
Sashmin <tolochko.minsk@gmail.com>
2022-11-17 11:53:40 +0300

    added writeToTxtFile function for Employee, rewrote Reporter with c-style I/O

--- Employee.h
+++ Employee.h
@@ -2,6 +2,7 @@
 #define EMPLOYEE_H
 #include <fstream>
 #include <iomanip>
+#include <stdio.h>
 
 struct Employee
 {
@@ -10,16 +11,18 @@ struct Employee
     double hours;
 };
 
-// std::istream& operator>>(std::istream& fin, Employee& emp)
-// {
-//     return fin >> emp.num >> emp.name >> emp.hours;
-// }
+std::istream& operator>>(std::istream& fin, Employee& emp)
+{
+    return fin >> emp.num >> emp.name >> emp.hours;
+}
 
-// std::ostream& operator<<(std::ostream& fout, const Employee& emp)
-// {
-//     return fout << emp.num << std::setw(20) << emp.name << std::setw(20) << emp.hours;
-// }
+std::ostream& operator<<(std::ostream& fout, const Employee& emp)
+{
+    return fout << emp.num << std::setw(20) << emp.name << std::setw(20) << emp.hours;
+}
 
-
+void writeEmpToTxtFile(Employee& emp, FILE* file){
+    fprintf(file, "%20d%20s%20.f", emp.num, emp.name, emp.hours);
+}
 
 #endif
--- Reporter.cpp
+++ Reporter.cpp
@@ -14,16 +14,22 @@ int binFileSize(std::string binFile)
 int main(int argc, char* argv[])
 {
     std::ifstream fin(argv[1], std::ios::binary);
-    std::ofstream fout(argv[2]);
+    //std::ofstream fout(argv[2]);
+    FILE *fout = fopen(argv[2], "r+");
     int numOfEmployees = binFileSize(argv[1]) / sizeof(Employee);
 
-    fout << "Report from " + std::string(argv[1]) << '\n';
-    fout << "ID" << std::setw(20) << "NAME" << std::setw(20) << "HOURS" << std::setw(20) << "TOTAL SALARY" << '\n';
+    // fout << "Report from " + std::string(argv[1]) << '\n';
+    // fout << "ID" << std::setw(20) << "NAME" << std::setw(20) << "HOURS" << std::setw(20) << "TOTAL SALARY" << '\n';
+
+    fprintf(fout, "Report from %s\n", argv[1]);
+    fprintf(fout, "                  ID                NAME               HOURS        TOTAL SALARY\n");
 
     for (int i = 0; i < numOfEmployees; i++)
     {
         Employee temp;
         fin.read((char*)&temp, sizeof(Employee));
-        fout << temp << std::setw(20) << temp.hours * atoi(argv[3]) << '\n';
+        //fout << temp << std::setw(20) << temp.hours * atoi(argv[3]) << '\n';
+        writeEmpToTxtFile(temp, fout);
+        fprintf(fout, "%20.f\n", temp.hours * atoi(argv[3]));
     }
 }
