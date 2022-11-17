3da1d5a6878e0c63eb2cfa3ed015674576f0c60d
Sashmin <tolochko.minsk@gmail.com>
2022-11-17 12:26:24 +0300

    rewrote binFleSize function in Reporter with c-style functions

--- Reporter.cpp
+++ Reporter.cpp
@@ -6,21 +6,17 @@
 
 int binFileSize(std::string binFile)
 {
-    std::ifstream fin(binFile, std::ios::binary);
-    fin.seekg(0, std::ios::end);
-    return fin.tellg();
+    const char* ch = binFile.c_str();
+    FILE *fin = fopen(ch, "r+b");
+    fseek(fin, 0, SEEK_END);
+    return ftell(fin);
 }
 
 int main(int argc, char* argv[])
 {
-    //std::ifstream fin(argv[1], std::ios::binary);
-    //std::ofstream fout(argv[2]);
     FILE *fin = fopen(argv[1], "r+b");
     FILE *fout = fopen(argv[2], "r+");
     int numOfEmployees = binFileSize(argv[1]) / sizeof(Employee);
-
-    // fout << "Report from " + std::string(argv[1]) << '\n';
-    // fout << "ID" << std::setw(20) << "NAME" << std::setw(20) << "HOURS" << std::setw(20) << "TOTAL SALARY" << '\n';
 
     fprintf(fout, "Report from %s\n", argv[1]);
     fprintf(fout, "                  ID                NAME               HOURS        TOTAL SALARY\n");
@@ -28,11 +24,10 @@ int main(int argc, char* argv[])
     for (int i = 0; i < numOfEmployees; i++)
     {
         Employee temp;
-        //fin.read((char*)&temp, sizeof(Employee));
-        //fout << temp << std::setw(20) << temp.hours * atoi(argv[3]) << '\n';
         fread(&temp, sizeof(Employee), 1, fin);
         writeEmpToTxtFile(temp, fout);
         fprintf(fout, "%20.f\n", temp.hours * atoi(argv[3]));
     }
     fclose(fout);
+    fclose(fin);
 }
