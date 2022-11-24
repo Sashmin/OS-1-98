776b7fee308a4ce7414e6b578f2a45c2db0682a3
Sashmin <tolochko.minsk@gmail.com>
2022-11-24 14:51:55 +0300

    rewrote Creator with c-style I/O

--- Creator.cpp
+++ Creator.cpp
@@ -4,13 +4,19 @@
 
 int main(int argc, char* argv[])
 {
-    std::ofstream fout(argv[1], std::ios::binary);
+    FILE* binOutputFile;
+    errno_t binOutputStream = fopen_s(&binOutputFile, argv[1], "wb");
     Employee temp;
     for (int i = 0; i < atoi(argv[2]); i++)
     {
-        std::cin >> temp;
-        fout.write((char*)&temp, sizeof(Employee));
-    }
+        printf_s("%d:\n", i + 1);
+        scanf_s("%d", &temp.num);
+        scanf_s("%s", temp.name, 10);
+        scanf_s("%lf", &temp.hours);
 
-    fout.close();
+        fwrite(&temp,
+             sizeof(Employee),
+             1,
+             binOutputFile);
+    }
 }
